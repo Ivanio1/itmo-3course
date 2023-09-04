a11390596d36904a044fde6e722b49f54d6fe7ea
Ivanio1 <sobolev1van@yandex.ru>
2023-09-04 18:18:40 +0300

    Fix SII lab1

--- Семестр 5/СИИ/lab1/lab1.pl
+++ Семестр 5/СИИ/lab1/lab1.pl
@@ -3,88 +3,140 @@
 
 /*Факты*/
 
+%Факты о том, какие команды в игре
+team(terrorists).
+team(counterterrorists).
+
+%Факты о том, что карта в игре
+active_map(de_dust2).
+active_map(de_mirage).
+active_map(dz_sirrocco).
+
 %Факты о том, что персонаж является игроком
-player(TheDoctorRomanov)
-player(NumberK).
-player(MrMuhlik).
-player(MichaelSyfers).
-player(JacquesBeltram).
-player(JohnKask).
+player(the_doctor_romanov).
+player(number_k).
+player(mr_muhlik).
+player(michael_syfers).
+player(jacques_beltram).
+player(john_kask).
 
 %Факты о том, что данное оружие в игре.
-active_weapon(AK47).
-active_weapon(AWP).
-active_weapon(P250).
-active_weapon(MAG7).
-active_weapon(Fire Grenade).
-
-
-
-%Факты о том, какие оружием является 
-weapon(AK47, rifle).
-weapon(AWP, rifle).
-weapon(P250, pistol).
-weapon(MAG7, shotgun).
-weapon(Fire Grenade, grenade).
+active_weapon(ak47).
+active_weapon(awp).
+active_weapon(p250).
+active_weapon(usp_s).
+active_weapon(mag7).
+active_weapon(xm1014).
+active_weapon(fire_grenade).
+active_weapon(smoke_grenade).
 
 %Факты о наличие оружия у игрока 
-character_weapon(TheDoctorRomanov, AK47).
-character_weapon(NumberK, P250).
-character_weapon(MrMuhlik, Fire Grenade).
-character_weapon(MichaelSyfers, AWP).
-character_weapon(JacquesBeltram, P250).
-character_weapon(JohnKask, MAG7).
+player_weapon(the_doctor_romanov, ak47).
+player_weapon(the_doctor_romanov, smoke_grenade).
+player_weapon(number_k, p250).
+player_weapon(mr_muhlik,fire_grenade).
+player_weapon(mr_muhlik,xm1014).
+player_weapon(michael_syfers, awp).
+player_weapon(jacques_beltram, p250).
+player_weapon(john_kask, mag7).
+
+%Факты убийства игроков во время игры
+kill(number_k,michael_syfers).
+kill(jacques_beltram,mr_muhlik).
+kill(number_k,jacques_beltram).
+kill(the_doctor_romanov,john_kask).
+
+%Факты о командах игроков
+player_team(the_doctor_romanov, terrorists).
+player_team(number_k, terrorists).
+player_team(mr_muhlik, terrorists).
+player_team(michael_syfers, counterterrorists).
+player_team(jacques_beltram, counterterrorists).
+player_team(john_kask, counterterrorists).
 
 
 
-%Предикаты
+/*Предикаты*/
 %Предикаты о типе оружия 
-%Пистолет является стрелковым оружием
-weapon_type(pistol, shooting).
+weapon_type(ak47, rifle).
+weapon_type(awp, rifle).
+weapon_type(p250, pistol).
+weapon_type(usp_s, pistol).
+weapon_type(mag7, shotgun).
+weapon_type(xm1014, shotgun).
+weapon_type(fire_grenade, grenade).
+weapon_type(smoke_grenade, grenade).
 
-%Автомат является стрелковым оружием
-weapon_type(rifle, shooting).
+%Предикаты об уроне оружия
+weapon_damage(ak47, 36).
+weapon_damage(awp, 115).
+weapon_damage(p250, 38).   
+weapon_damage(mag7,30).
+weapon_damage(usp_s, 35). 
+weapon_damage(xm1014,20).
 
-%Дробовик является стрелковым оружием
-weapon_type(shotgun, shooting).
+%Предикаты о количестве патронов в оружии
+weapon_magazine(ak47, 30).
+weapon_magazine(awp, 5).
+weapon_magazine(p250, 13).   
+weapon_magazine(mag7, 5).
+weapon_magazine(usp_s, 12). 
+weapon_magazine(xm1014,7).
 
-%Граната является разрывным оружием
-weapon_type(grenade, explosive).
-
-
-%Предикаты о типе карт
+%Предикаты о типе игровых карт
 map_type(de_dust2, competitive).
 map_type(de_mirage, competitive).
-map_type(ar_dizzy, flying scoutsman).
-map_type(dz_sirrocco, danger zone).
-map_type(de_inferno,competitive).
-map_type(dz_vineyard, danger zone).
-
-
-%Предикаты о командах игроков
-character_team(TheDoctorRomanov, terrorists).
-character_team(NumberK, terrorists).
-character_team(MrMuhlik, terrorists).
-character_team(MichaelSyfers, counterterrorists).
-character_team(JacquesBeltram, counterterrorists).
-character_team(JohnKask, counterterrorists).
+map_type(ar_dizzy, flying_scoutsman).
+map_type(dz_sirrocco, danger_zone).
+map_type(de_inferno, competitive).
+map_type(dz_vineyard, danger_zone).
 
 
 
-
-%Правила
+/*Правила*/
 %Правило о принадлежности к террористам
+is_terrorist(Character):-
+     player(Character),
+     player_team(Character, terrorists).
 
 %Правило о принадлежности к контртеррористам
-
-%Правило о возможности убить
+is_counterterrorist(Character):-
+     player(Character),
+     player_team(Character, counterterrorists).
 
 %Правило о наличии дробовика
-is_character_has_shotgun(Character, Weapon):-
-	character_weapon(Character, Weapon), 
-	weapon(Weapon, shotgun).
+is_character_has_shotgun(Character):-
+    player(Character),
+	player_weapon(Character,Weapon),
+    weapon_type(Weapon,shotgun).
 
-%Правило, проверяющее является ли карта соревновательной
+%Правило о наличии пистолета
+is_character_has_pistol(Character):-
+    player(Character),
+	player_weapon(Character,Weapon),
+    weapon_type(Weapon,pistol).	
 
+%Правило о наличии автомата
+is_character_has_rifle(Character):-
+    player(Character),
+	player_weapon(Character,Weapon),
+    weapon_type(Weapon,rifle).	
+
+%Правило, проверяющее является ли карта соревновательной и активной
+is_map_competitive(Map):-
+    active_map(Map),
+	map_type(Map,competitive).
+
+%Правило, проверяющее возможность бросить определённую гранату
+ability_to_throw_grenade(Character, Grenade):-
+    player(Character),
+    player_weapon(Character,Grenade).	
+
+%Правило, проверяющее есть ли в команде гранаты
+has_team_grenades(Team):-
+    team(Team),
+    player_team(Character, Team),
+    player_weapon(Character,Weapon),
+     weapon_type(Weapon,grenade).
 
 
