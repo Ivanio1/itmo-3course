0b359a48e2d31d456857218d341077f6e4276ff1
Ivanio1 <sobolev1van@yandex.ru>
2023-10-21 16:28:33 +0300

    Done isbd4

---
+++ Семестр 5/ИСБД/lab4/sql/index.sql
@@ -0,0 +1,9 @@
+create index points on м_нжемйх using hash(йнд);
+create index statements on м_беднлнярх using btree(вкбй_хд);
+
+
+create index chlvk_id_index on м_насвемхъ using hash(вкбй_хд);
+create index name_index on м_кчдх using btree(хлъ);
+create index nzk_index on м_насвемхъ using btree(мгй);
+
+
