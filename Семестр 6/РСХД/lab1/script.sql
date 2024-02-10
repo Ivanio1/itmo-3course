1269e1fa6315cbf4417ef6681cd9b14fb08e3b79
Ivanio1 <sobolev1van@yandex.ru>
2024-02-10 12:26:59 +0300

    Start RSHD1

---
+++ Семестр 6/РСХД/lab1/script.sql
@@ -0,0 +1,23 @@
+DO $$
+DECLARE
+    row     record;
+    from_user text := 's336760';
+    to_user text := 's111111';
+    table_num int := 1;
+    name_of_table text;
+BEGIN
+    RAISE INFO 'Текущий пользователь: %', from_user;
+    RAISE INFO 'Кому выдаём права доступа: %', to_user;
+    RAISE INFO 'No.  Имя таблицы';
+    RAISE INFO '---  -------------------------------';
+
+    FOR row IN 
+        SELECT DISTINCT table_name as name_of_table
+        FROM information_schema.role_table_grants rg
+        JOIN pg_tables pt ON rg.table_schema = pt.schemaname AND rg.table_name = pt.tablename
+        WHERE grantee = from_user
+        LOOP
+        	RAISE INFO '% %', table_num, row.name_of_table;
+        	table_num := table_num + 1;
+    	END LOOP;
+END $$;
---
+++ Семестр 6/РСХД/lab1/start.sh
@@ -0,0 +1,7 @@
+\prompt 'Введите имя текущего пользователя: ' current_user_name
+
+\set  current_user_name '\'' :current_user_name '\''
+
+\prompt 'Введите имя пользователя, кому нужно выдать права доступа: ' target_user_name
+
+\set target_user_name '\'' :target_user_name '\''
