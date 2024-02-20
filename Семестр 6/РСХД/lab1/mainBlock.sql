75b66511b8374e06c8e0fd5430beb1e9af446b5a
Ivanio1 <sobolev1van@yandex.ru>
2024-02-20 14:17:32 +0300

    Fix users

--- Семестр 6/РСХД/lab1/mainBlock.sql
+++ Семестр 6/РСХД/lab1/mainBlock.sql
@@ -1,11 +1,35 @@
 DO $$
 DECLARE
     row     record;
-    from_user text := 's100000';
-    to_user text := 's111111';
+    from_user text := 's336760';
+    to_user text := 's119391';
     table_num int := 1;
     name_of_table text;
+    from_user_exists boolean := false;
+    to_user_exists boolean := false;
 BEGIN
+    FOR row IN 
+        SELECT usename
+        FROM pg_user
+    LOOP
+        IF row.usename = from_user THEN
+            from_user_exists := true;
+        END IF;
+        
+        IF row.usename = to_user THEN
+            to_user_exists := true;
+        END IF;
+    END LOOP;
+    
+    IF from_user_exists = false THEN
+        RAISE INFO 'Пользователь % на выдачу прав не найден', from_user;
+    END IF;
+    
+    IF to_user_exists = false THEN
+        RAISE INFO 'Пользователь % на получение прав не найден', to_user;
+	RETURN;
+    END IF;
+
     RAISE INFO 'Текущий пользователь: %', from_user;
     RAISE INFO 'Кому выдаём права доступа: %', to_user;
     RAISE INFO 'No.  Имя таблицы';
@@ -16,8 +40,8 @@ BEGIN
         FROM information_schema.role_table_grants rg
         JOIN pg_tables pt ON rg.table_schema = pt.schemaname AND rg.table_name = pt.tablename
         WHERE grantor = from_user and is_grantable = 'YES'
-        LOOP
-        	RAISE INFO '% %', table_num, row.name_of_table;
-        	table_num := table_num + 1;
-    	END LOOP;
+    LOOP
+        RAISE INFO '% %', table_num, row.name_of_table;
+        table_num := table_num + 1;
+    END LOOP;
 END $$;
--- Семестр 6/РСХД/lab1/procedure.sql
+++ Семестр 6/РСХД/lab1/procedure.sql
@@ -4,7 +4,31 @@ DECLARE
     row     record;
     table_num int := 1;
     name_of_table text;
+    from_user_exists boolean := false;
+    to_user_exists boolean := false;
 BEGIN
+    FOR row IN 
+        SELECT usename
+        FROM pg_user
+    LOOP
+        IF row.usename = from_user THEN
+            from_user_exists := true;
+        END IF;
+        
+        IF row.usename = to_user THEN
+            to_user_exists := true;
+        END IF;
+    END LOOP;
+    
+    IF from_user_exists = false THEN
+        RAISE INFO 'Пользователь % на выдачу прав не найден', from_user;
+    END IF;
+    
+    IF to_user_exists = false THEN
+        RAISE INFO 'Пользователь % на получение прав не найден', to_user;
+	RETURN;
+    END IF;
+
     RAISE INFO 'Текущий пользователь: %', from_user;
     RAISE INFO 'Кому выдаём права доступа: %', to_user;
     RAISE INFO 'No.  Имя таблицы';
@@ -15,9 +39,9 @@ BEGIN
         FROM information_schema.role_table_grants rg
         JOIN pg_tables pt ON rg.table_schema = pt.schemaname AND rg.table_name = pt.tablename
         WHERE grantor = from_user and is_grantable = 'YES'
-        LOOP
-        	RAISE INFO '% %', table_num, row.name_of_table;
-        	table_num := table_num + 1;
-    	END LOOP;
+    LOOP
+        RAISE INFO '% %', table_num, row.name_of_table;
+        table_num := table_num + 1;
+    END LOOP;
 END; $$
 LANGUAGE 'plpgsql';
